e1ff9f442349893f5722f641bb892b6a0c0437aa
ShreehariVaasishta <shreehari9481@gmail.com>
2022-01-03 22:28:49 +0530

    feat: Fix template.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI
+import os
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
@@ -16,18 +17,23 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.post("/upload")
-def upload_file(html_content: str, file: bytes = File(...)):
+async def upload_file(request: Request):
     """
     Upload a csv file and print the contents as dictionary
     """
+    # delete output folder
+    os.system("rm -rf output")
+    request_body = await request.form()
+    html_content = request_body["html_content"]
+    csv_file = await request_body["file"].read()
+
     html_content = html_content.strip()
-    x = csv_to_dict(file)
+    x = csv_to_dict(csv_file)
     processed_csv = [
         {f"{i['name']}": replace_message_context_with_html(html_content, i)} for i in x
     ]
     html_str_to_pdf(processed_csv)
     output_zip = zip_output_pdf_files()
-
     return FileResponse(output_zip, media_type="application/zip")
 
 
